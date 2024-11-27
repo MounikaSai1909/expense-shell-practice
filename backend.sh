012fe3b186f45cf273b661f5954e227751277ab4
Mounika <mounikasaiaarepu09@gmail.com>
2024-11-27 22:01:56 +0530

    some-message

--- backend.sh
+++ backend.sh
@@ -46,4 +46,14 @@ fi
 mkdir -p /app
 VALIDATE $? "creating app directory"
 
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "downloading backend code"
+
+cd /app
+unzip /tmp/backend.zip
+VALIDATE $? "extracting backend code"
+
+npm install
+VALIDATE $? "install nodejs dependencies"
+
 
