088bf24b6f3907acb0e0c11dd27242ee7f654650
Mounika <mounikasaiaarepu09@gmail.com>
2024-12-01 15:27:20 +0530

    some-message

--- mysql.sh
+++ mysql.sh
@@ -39,7 +39,7 @@ VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting MySQL Server"
 
-mysql -h db.mounka.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.mounka.shop -uroot -p${mysql_root_password} -e 'SHOW DATABASES;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
